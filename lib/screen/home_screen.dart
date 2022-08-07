e903f5b7370234a67a4473afd0ae978dd7b5574a
kankanit saengthao <nut57130@gmail.com>
2022-08-07 10:43:50 +0700

    ทดสอบสร้างตัวเเปร

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -9,6 +9,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String name = "Bundit nuntates";
+  int age = 35;
+  bool lookingGood = true;
+
+  List<String> myDognames = ["loongtoo", "Paveena", "mike", "Pravit"];
+
+  Map<String, dynamic> user = {
+    "id": 1,
+    "name": "john Doe",
+    "sex": "male",
+    "married": false
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,17 +29,31 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text("Home Screen"),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () => {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) {
-                  return AboutScreen();
-                },
-              ),
-            )
-          },
-          child: Text("Go to about screen"),
+        child: Column(
+          children: [
+            Text("สวัสดีฉันชื่อ $name"),
+            Text("ฉันอายุ $age"),
+            Text(
+                "ฉันหล่อมากนั้นเป็นเรื่อง ${lookingGood ? "จริง" : "ไม่จริง"}"),
+            Text(
+                "หมาฉันชื่อ ${myDognames[0]},${myDognames[1]},${myDognames[2]},${myDognames[3]}"),
+            //...myDognames.map((item) => Text(item)).toList(),
+            Text("เพื่อนของฉันชื่อ: ${user["name"]}"),
+            Text(
+                "เเล้วเขาก็: ${user["married"] ? "เเต่งงานเเล้ว" : "ยังไม่ได้เเต่งงาน"}"),
+            ElevatedButton(
+              onPressed: () => {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return AboutScreen();
+                    },
+                  ),
+                )
+              },
+              child: Text("Go to about screen"),
+            ),
+          ],
         ),
       ),
     );
